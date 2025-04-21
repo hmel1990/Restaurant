30d34a13ad13c5961ddc5c6533202b11f9b8cb59
hmel1990 <hmel408757595@gmail.com>
2025-04-21 14:20:48 +0300

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class UdpChatClient
+{
+    private const int serverPort = 9000;
+    private UdpClient? client;
+    private IPEndPoint? serverEndpoint;
+    private string menu = "Выберите из списка, что хотетите заказать: \r\n\r\n" +
+        "1.Утка по-французски с апельсиновым соусом (Canard à l'orange)\r\n\r\n" +
+        "2.Паэлья с морепродуктами (Paella de Mariscos)\r\n\r\n" +
+        "3.Бефстроганов по-русски (Boeuf Stroganoff)\r\n\r\n" +
+        "4/Итальянская лазанья с мясом (Lasagna al Forno)\r\n\r\n" +
+        "5.Французский луковый суп (Soupe à l'oignon)\r\n\r\n" +
+        "6.Медальоны из телятины с грибным соусом\r\n\r\n" +
+        "7.Ризотто с белыми грибами (Risotto ai Funghi Porcini)\r\n\r\n" +
+        "8.Карпаччо из говядины с пармезаном\r\n\r\n" +
+        "9.Запечённый камамбер с мёдом и орехами\r\n\r\n" +
+        "10.Тапас ассорти (разные испанские закуски)";
+
+    public async Task StartAsync()
+    {
+        Console.Title = "CLIENT SIDE";
+        var serverIp = "127.0.0.1";
+        serverEndpoint = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+
+        client = new UdpClient(0);
+        await ConnectToServerAsync();
+
+        AppDomain.CurrentDomain.ProcessExit += async (s, e) => await SendDisconnectMessageAsync();
+
+        _ = Task.Run(ReceiveMessagesAsync);
+        await SendMessagesAsync();
+    }
+
+    private async Task ConnectToServerAsync()
+    {
+        while (true)
+        {
+            try
+            {
+                client.Connect(serverEndpoint);
+                await SendInitialMessageAsync();
+                Console.WriteLine("Подключено к серверу.");
+                break;
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Ожидание подключения к серверу...");
+                await Task.Delay(1000);
+            }
+        }
+    }
+
+    private async Task SendInitialMessageAsync()
+    {
+        var initialMessage = "Клиент подключился";
+        var data = Encoding.UTF8.GetBytes(initialMessage);
+        await client.SendAsync(data, data.Length);
+    }
+
+    private async Task SendDisconnectMessageAsync()
+    {
+        var disconnectMessage = "off";
+        var data = Encoding.UTF8.GetBytes(disconnectMessage);
+        await client.SendAsync(data, data.Length);
+    }
+
+    private async Task ReceiveMessagesAsync()
+    {
+        while (true)
+        {
+            var result = await client.ReceiveAsync();
+            var message = Encoding.UTF8.GetString(result.Buffer);
+            Console.WriteLine(message);
+        }
+    }
+
+    private async Task SendMessagesAsync()
+    {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(menu);
+            Console.ResetColor();
+
+        while (true)
+        {
+            var message = Console.ReadLine();
+            var data = Encoding.UTF8.GetBytes(message);
+            await client.SendAsync(data, data.Length);
+
+            if (message == "off")
+                break;
+        }
+
+        client.Close();
+        Console.WriteLine("Отключено от сервера.");
+    }
+
+    static async Task Main() => await new UdpChatClient().StartAsync();
+}
---
+++ Server/Program.cs
@@ -0,0 +1,217 @@
+﻿using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class UdpChatServer
+{
+    private const int port = 9000;
+    private UdpClient? server;
+    private ConcurrentDictionary<IPEndPoint, bool> clients = new();
+    private ConcurrentDictionary<IPEndPoint, int> clientsWithId = new();
+    private StringBuilder messageHistory = new();
+    private int clientCounter = 0;
+
+    private static readonly object fileLock = new object();
+
+    private static readonly string logFilePath = "E:\\STEP\\WebProg\\Restaurant\\Server\\bin\\Debug\\net9.0\\server_log.txt";
+
+    private static readonly ConcurrentQueue<string> logQueue = new();
+    private static readonly CancellationTokenSource logCts = new();
+    private static readonly Task logTask = Task.Run(() => ProcessLogQueue());
+    private static async Task ProcessLogQueue()
+    {
+        while (!logCts.Token.IsCancellationRequested)
+        {
+            try
+            {
+                if (logQueue.TryDequeue(out string logEntry))
+                {
+                    lock (fileLock)
+                    {
+                        File.AppendAllText(logFilePath, logEntry + Environment.NewLine);
+                    }
+                }
+                else
+                {
+                    await Task.Delay(50, logCts.Token); // ждём немного, если очередь пуста
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при записи лога: {ex.Message}");
+            }
+        }
+    }
+
+
+    public async Task StartAsync()
+    {
+        Console.Title = "SERVER SIDE";
+        await InitializeServerAsync();
+
+        _ = Task.Run(ReceiveMessagesAsync);
+        await HandleConsoleInputAsync();
+    }
+
+    private async Task InitializeServerAsync()
+    {
+        while (true)
+        {
+            try
+            {
+                server = new UdpClient(port);
+                Console.WriteLine($"Сервер запущен на порту {port}.");
+                break;
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Не удалось запустить сервер. Ожидание повторного подключения...");
+                await Task.Delay(1000);
+            }
+        }
+    }
+
+    private Dictionary<int, string> menuItems = new Dictionary<int, string>
+{
+    { 1, "Утка по-французски с апельсиновым соусом (Canard à l'orange)" },
+    { 2, "Паэлья с морепродуктами (Paella de Mariscos)" },
+    { 3, "Бефстроганов по-русски (Boeuf Stroganoff)" },
+    { 4, "Итальянская лазанья с мясом (Lasagna al Forno)" },
+    { 5, "Французский луковый суп (Soupe à l'oignon)" },
+    { 6, "Медальоны из телятины с грибным соусом" },
+    { 7, "Ризотто с белыми грибами (Risotto ai Funghi Porcini)" },
+    { 8, "Карпаччо из говядины с пармезаном" },
+    { 9, "Запечённый камамбер с мёдом и орехами" },
+    { 10, "Тапас ассорти (разные испанские закуски)" }
+};
+    private async Task ForSwitchCase(int menuNumber, IPEndPoint client, int ID)
+    {
+        var formattedMessage = $"\n{client} (клиент № {ID}): заказал {menuItems[menuNumber]}";
+        messageHistory.AppendLine(formattedMessage);
+        var logEntry = $"{DateTime.Now}: {formattedMessage}";
+        logQueue.Enqueue(logEntry);
+        await BroadcastMessageAsync(formattedMessage, client);
+
+        _ = Task.Run(async () =>
+        {
+            await Task.Delay(10000);
+            var readyMessage = $"\n{client} {menuItems[menuNumber]} (клиента № {ID}) готова";
+            messageHistory.AppendLine(readyMessage);
+            logEntry = $"{DateTime.Now}: {readyMessage}";
+            logQueue.Enqueue(logEntry);
+            await BroadcastMessageAsync(readyMessage, client);
+        });
+    }
+
+    private async Task ReceiveMessagesAsync()
+    {
+        while (true)
+        {
+            var result = await server.ReceiveAsync();
+            var message = Encoding.UTF8.GetString(result.Buffer);
+
+            if (!clients.ContainsKey(result.RemoteEndPoint))
+            {
+                clients[result.RemoteEndPoint] = true;
+                clientCounter++;
+                clientsWithId[result.RemoteEndPoint] = clientCounter;
+                await SendHistoryAsync(result.RemoteEndPoint);
+                Console.WriteLine($"\nКлиент подключился: {result.RemoteEndPoint} (Клиент #{clientCounter})");
+                var logEntry = $"{DateTime.Now}: Клиент подключился: {result.RemoteEndPoint} (Клиент #{clientCounter})";
+                logQueue.Enqueue(logEntry);
+
+            }
+
+            if (message == "off")
+            {
+                clients.TryRemove(result.RemoteEndPoint, out _);
+                //clientsWithId.TryRemove(result.RemoteEndPoint, out _); // Не удаляем ID клиента, чтобы сохранить № заказа-клиента
+                Console.WriteLine($"\nКлиент отключился: {result.RemoteEndPoint}");
+                var logEntry = $"{DateTime.Now}: Клиент отключился: {result.RemoteEndPoint} (Клиент #{clientCounter})";
+                logQueue.Enqueue(logEntry);
+                continue;
+            }
+
+            var clienetID = clientsWithId[result.RemoteEndPoint];
+            //var formattedMessage = $"\n{result.RemoteEndPoint} (клиент № {clienetID}): {message}";
+
+            var formattedMessage="";
+            switch (message)
+            {
+                case "1":
+                    await ForSwitchCase(1, result.RemoteEndPoint, clienetID);
+                    break;
+
+                case "2":
+                    await ForSwitchCase(1, result.RemoteEndPoint, clienetID);
+                    break;
+
+                case "3":
+                    await ForSwitchCase(1, result.RemoteEndPoint, clienetID);
+                    break;
+
+                case "4":
+                    await ForSwitchCase(1, result.RemoteEndPoint, clienetID);
+                    break;
+
+                case "5":
+                    await ForSwitchCase(1, result.RemoteEndPoint, clienetID);
+                    break;
+
+                case "6":
+                    await ForSwitchCase(1, result.RemoteEndPoint, clienetID);
+                    break;
+
+                case "7":
+                    await ForSwitchCase(1, result.RemoteEndPoint, clienetID);
+                    break;
+
+                case "8":
+                    await ForSwitchCase(1, result.RemoteEndPoint, clienetID);
+                    break;
+
+                case "9":
+                    await ForSwitchCase(1, result.RemoteEndPoint, clienetID);
+                    break;
+
+                case "10":
+                    await ForSwitchCase(1, result.RemoteEndPoint, clienetID);
+                    break;
+            }
+
+        }
+
+    }
+
+    private async Task SendHistoryAsync(IPEndPoint client)
+    {
+        var history = Encoding.UTF8.GetBytes(messageHistory.ToString());
+        await server.SendAsync(history, history.Length, client);
+    }
+
+    private async Task BroadcastMessageAsync(string message, IPEndPoint? excludeClient = null)
+    {
+        var data = Encoding.UTF8.GetBytes(message);
+        foreach (var client in clients.Keys)
+        {
+            //if (!client.Equals(excludeClient)) // Клиент тоже должен видеть себя в обще очереди
+                await server.SendAsync(data, data.Length, client);
+        }
+    }
+
+    private async Task HandleConsoleInputAsync()
+    {
+        while (true)
+        {
+            Console.Write("Отправьте сообщение клиентам: ");
+            var input = Console.ReadLine();
+            var formattedMessage = $"\nСервер: {input}";
+            messageHistory.AppendLine(formattedMessage);
+            await BroadcastMessageAsync(formattedMessage);
+        }
+    }
+
+    static async Task Main() => await new UdpChatServer().StartAsync();
+}
